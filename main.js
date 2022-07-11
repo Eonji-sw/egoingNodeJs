c85a5e60238b588d68f0fe170b489552e77be914
Eonji-sw <ejkim0625@gmail.com>
2022-07-12 04:07:59 +0900

    templateHTML 함수와 templateList 함수를 이용해서 코드 정리

--- main.js
+++ main.js
@@ -2,6 +2,34 @@ var http = require('http');
 var fs = require('fs'); // 모듈 fs : nodejs의 모듈인 파일 시스템
 var url = require('url'); // 모듈 url
 
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i += 1;
+  }
+  list = list + '</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url; // query
     // url.parse(_url, true) : 주어진 url 정보를 분석
@@ -13,57 +41,17 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', (err, filelist)=>{
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i += 1;
-          }
-          list = list + '</ul>';
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200); // 파일을 성공적으로 전송
           response.end(template);
         })
       } else {
         fs.readdir('./data', (err, filelist)=>{
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i += 1;
-          }
-          list = list + '</ul>';
           fs.readFile(`data/${queryData.id}`, 'utf8', (err, description)=>{
             var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200); // 파일을 성공적으로 전송
             response.end(template);
           });
@@ -73,6 +61,5 @@ var app = http.createServer(function(request,response){
       response.writeHead(404); // 파일을 찾을 수 없는 경우
       response.end('Not found');
     }
-
 });
 app.listen(3000);

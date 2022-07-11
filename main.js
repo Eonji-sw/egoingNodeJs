117220c1c24f427e16b7aed0819b3cab97809134
Eonji-sw <ejkim0625@gmail.com>
2022-07-12 03:24:09 +0900

    not found 오류 구현

--- main.js
+++ main.js
@@ -4,36 +4,40 @@ var url = require('url'); // 모듈 url
 
 var app = http.createServer(function(request,response){
     var _url = request.url; // query
+    // url.parse(_url, true) : 주어진 url 정보를 분석
     var queryData = url.parse(_url, true).query; // query 객체 -> { id : 값 }
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-    if(_url == '/'){ // 홈으로 갔을 때
-      title = 'Welcome';
+
+    if(pathname === '/'){
+      fs.readFile(`data/${queryData.id}`, 'utf8', (err, description)=>{
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200); // 파일을 성공적으로 전송
+        response.end(template);
+      });
+    } else {
+      response.writeHead(404); // 파일을 찾을 수 없는 경우
+      response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', (err, description)=>{
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    })
+
+
 });
 app.listen(3000);

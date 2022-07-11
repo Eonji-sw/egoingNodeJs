abee3850badba928527b160f13abe0840f15b54b
Eonji-sw <ejkim0625@gmail.com>
2022-07-12 05:03:02 +0900

    form UI 출력(create 페이지 생성)

--- main.js
+++ main.js
@@ -13,13 +13,14 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
   `;
 }
 
-function templateList(filelist){
+function templateList(filelist){ // 글 목록
   var list = '<ul>';
   var i = 0;
   while(i < filelist.length) {
@@ -57,6 +58,24 @@ var app = http.createServer(function(request,response){
           });
         });
       }
+    } else if(pathname === '/create') {
+      fs.readdir('./data', (err, filelist)=>{
+        var title = 'WEB - create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/process_create" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `);
+        response.writeHead(200); // 파일을 성공적으로 전송
+        response.end(template);
+      })
     } else {
       response.writeHead(404); // 파일을 찾을 수 없는 경우
       response.end('Not found');

ea1f6ef3711a14e2bec5c935b010358972aa0fb0
Eonji-sw <ejkim0625@gmail.com>
2022-07-12 02:12:43 +0900

    query string에 따라 다른 정보 출력

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url'); // 모듈 url
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url; // query
+    var queryData = url.parse(_url, true).query; // 객체 -> { id : 값 }
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id);
 
 });
 app.listen(3000);

f9fbc30cb36d94e29974b91d28adc18f203a5d41
Eonji-sw <ejkim0625@gmail.com>
2022-07-12 03:31:06 +0900

    조건문 사용하여 주소에 따라 다르게 동작하는 application 구현

--- main.js
+++ main.js
@@ -7,32 +7,60 @@ var app = http.createServer(function(request,response){
     // url.parse(_url, true) : 주어진 url 정보를 분석
     var queryData = url.parse(_url, true).query; // query 객체 -> { id : 값 }
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
 
     if(pathname === '/'){
-      fs.readFile(`data/${queryData.id}`, 'utf8', (err, description)=>{
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200); // 파일을 성공적으로 전송
-        response.end(template);
-      });
+      if(queryData.id === undefined) { // home일 경우
+        fs.readFile(`data/${queryData.id}`, 'utf8', (err, description)=>{
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200); // 파일을 성공적으로 전송
+          response.end(template);
+        });
+      } else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', (err, description)=>{
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200); // 파일을 성공적으로 전송
+          response.end(template);
+        });
+      }
     } else {
       response.writeHead(404); // 파일을 찾을 수 없는 경우
       response.end('Not found');
